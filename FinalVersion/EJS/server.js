2641b15c5dee50e9f7df0ddea24d1e249d5b753d
Calvin Tran <tmp.calvin@gmail.com>
2023-11-26 18:07:38 -0500

    added listener

--- FinalVersion/EJS/server.js
+++ FinalVersion/EJS/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/radiostation');
+mongoose.connect('mongodb+srv://username:PassWord@cluster0.0jxtibh.mongodb.net/');
 const db = mongoose.connection;
 db.once('open', () => {
   console.log('connected to mongo');
@@ -53,7 +53,97 @@ app.get('/', function (req, res) {
     });
 });
 //------------------------------------------------------------------------------------- listner
+const referencSchema= new mongoose.Schema({ref:String, bool:String});
+const References = mongoose.model('refs', referencSchema);
 
+// main page
+app.get('/listener_main', async function(req, res) {
+  
+  let array = []
+  const reff = await References.find({"bool": "1"}, {ref:1, _id:0})
+  reff.forEach((element) => array = array.concat(Object.values(Object.values(element)[2]))) 
+  const first = reff[0]
+
+
+  const playlist = await DJ.find({"Playlists.artist": {$in:array}})
+  const songs = await Song.find()
+  const artists = await References.find()
+
+  // res.render('pages/listener_main', {music: empty});
+  res.render('pages/listener_main', {music: songs, reference:artists, playlist: playlist});
+});
+app.post('/listener_main', async function(req, res) {
+  
+  if(req.body.search){
+
+    const keyword = req.body.search;
+    const filter = {"artist":keyword};
+    const results = await Song.find(filter);
+    if(results.length == 0){
+      results.push({artist: 'NOT', title: "FOUND"});
+    }
+    // res.render('pages/listener_music', {data: epty});
+    res.render('pages/listener_music', {data: results});
+   
+  }
+  if(req.body.ref){
+    const updated = req.body;
+    const filter = {"ref":updated.ref};
+    
+    const inserted = await References.findOneAndUpdate(filter, updated);
+    const songs = await Song.find()
+    const artists = await References.find()
+
+    let array = []
+    const reff = await References.find({"bool": "1"}, {ref:1, _id:0})
+    reff.forEach((element) => array = array.concat(Object.values(Object.values(element)[2]))) 
+    
+
+    const playlist = await DJ.find({"Playlists.artist": {$in:array}})
+    
+    // res.render('pages/listener_main', {music: epty});
+    res.render('pages/listener_main', {music: songs, reference:artists, playlist: playlist});
+
+  }
+  else{
+    const updated = req.body;
+    
+  const filter = {"_id":updated._id};
+  let updatedUser = await Song.findOneAndUpdate(filter, updated);
+  const songs = await Song.find()
+  const artists = await References.find()
+
+  let array = []
+    const reff = await References.find({"bool": "1"}, {ref:1, _id:0})
+    reff.forEach((element) => array = array.concat(Object.values(Object.values(element)[2]))) 
+    
+
+    const playlist = await DJ.find({"Playlists.artist": {$in:array}})
+    
+    // res.render('pages/listener_main', {music: epty});
+    res.render('pages/listener_main', {music: songs, reference:artists, playlist: playlist});
+  }
+ 
+});
+
+
+
+
+
+app.get('/listener_profile', async function(req, res) {
+  const data = await AAA.find();
+
+  // res.render('pages/listener_profile', {user: empty});
+  res.render('pages/listener_profile', {user: data});
+ 
+});
+app.post('/listener_profile', async function(req, res) {
+  const updated = req.body;
+  const filter = {"_id":updated._id};
+  let updatedUser = await AAA.findOneAndUpdate(filter, updated);
+  // res.render('pages/listener_profile', {user: empty});
+  res.render('pages/listener_profile', {user: updatedUser});
+});
 //------------------------------------------------------------------------------------- DJ
 
 
